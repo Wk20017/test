80ee1c428263c78a99d77c1a06d443f52dc507dd
Wk20017 <wkk972509350@gmail.com>
2020-09-10 19:46:35 +0800

    2020.9.10 19.46

--- Mydata.py
+++ Mydata.py
@@ -5,7 +5,10 @@ import pandas as pd
 # df = pd.read_csv('data1.csv', names=['a', 'b', 'c', 'd', 'info'])
 # df = pd.read_table('data1.csv', sep=',')
 # name = ['A1', 'A2', 'A3', 'A4', 'info']
-df = pd.read_csv('data2.csv')
+# df = pd.read_csv('data2.csv')
+# print(df)
+# df.to_csv('data3.csv', na_rep='Null', header=False, index=False)
+# df1 = pd.read_csv('data3.csv')
+# print(df1)
+df = pd.read_excel('C:/Users/王凯/Desktop/data4.xlsx', header=None, names=['a', 'b', 'c'])
 print(df)
-# this is just a test
-＃这是我的第二个测试
