25a104ccc528e30d24df4e12cb09580c28d5cc85
Wk20017 <wkk972509350@gmail.com>
2020-09-11 23:13:31 +0800

    2020.9.11 23:13

--- Mydata.py
+++ Mydata.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import json
 # df = pd.read_csv('data1.csv')
 # df = pd.read_csv('data1.csv', header=None)
 # df = pd.read_csv('data1.csv', names=['a', 'b', 'c', 'd', 'info'])
@@ -10,5 +11,32 @@ import pandas as pd
 # df.to_csv('data3.csv', na_rep='Null', header=False, index=False)
 # df1 = pd.read_csv('data3.csv')
 # print(df1)
-df = pd.read_excel('C:/Users/王凯/Desktop/data4.xlsx', header=None, names=['a', 'b', 'c'])
-print(df)
+# df = pd.read_excel('C:/Users/王凯/Desktop/data4.xlsx', header=None, names=['a', 'b', 'c'])
+# print(df)
+# print('----------------')
+# print(df.loc[:1, ['a', 'b']])
+# df = pd.read_excel('C:/Users/王凯/Desktop/data4.xlsx', header=None)
+# new_df = df.loc[0:2]
+# print(df)
+# print(type(new_df))
+# new_df.to_excel('C:/Users/王凯/Desktop/data4.xlsx', sheet_name='Sheet3')
+# obj = """
+# {"name": "Wes",
+#     "places_lived": ["United States", "Spain", "Germany"],
+#     "pet": null,
+#     "siblings": [{"name": "Scott", "age": 30, "pets": ["Zeus", "Zuko"]},
+#                  {"name": "Katie", "age": 38, "pets": ["Sixes", "Stache", "Cisco"]}]
+# }
+# """
+# print(type(obj))
+# result = json.loads(obj)
+# print(type(result), result)
+# print('-------------')
+# change = json.dumps(result)
+# print(type(change), change)
+# print('-------------')
+# df = pd.DataFrame(result['siblings'], columns=['name', 'age', 'pets'])
+# print(df)
+# df.to_json('test.json')
+# df = pd.read_json('test.json')
+# print(df)
---
+++ my_data_clean.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+# df = pd.DataFrame(np.random.randn(10, 6))
+# df.iloc[:4, 1] = None
+# df.iloc[:2, 4:6] = None
+# df.iloc[6, 3:5] = None
+# df.iloc[8, 0:2] = None
+#
+# print(df)
+# print('---------------')
+# print(df.dropna(thresh=4)) #保留至少有4个非空值的行
+# print('---------------')
+# print(df.dropna(subset=[2, 4]))
+# print('---------------')
+# info = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5} #用指定值填充指定列的NaN，如用0填充第0列
+# print(df.fillna(value=info))
+# result = df.isnull()
+# print(result)
+# result = df.isnull().any()
+# print(result)
+# result = df[df.isnull().values == True].drop_duplicates()  #去掉重复行
+# print('------------------')
+# print(result)
+
+# df = pd.DataFrame({'k1': ['one', 'two']*3 + ['two']*2, 'k2': [1, 2, 3, 4, 5, 6, 7, 8]})
+# print(df)
+# print('-------------------------')
+# print(df.duplicated(keep=False))
+# print(df.duplicated(subset='k1', keep=False))
+# print('--------------------------')
+# print(df.drop_duplicates(subset='k1'))
+
+# df = pd.DataFrame(np.random.randn(1000, 4))
+# print(df.describe())
+# col = df[2]
+# print(df[(np.abs(df)>3).any(1)])
+
+# x = (lambda a= 'fee', b= 'bee', c= 'aee' : a+b+c)
+# print(type(x))
+# print(x('my'))
+
+# key = 'got'
+# a = {
+#     'already': (lambda : 2 + 2),
+#     'got': (lambda : 2 * 4),
+#     'one': (lambda : 2 ** 6)
+# }[key]()
+# print(a)
+
+# action = (lambda x: (lambda y: x+y))
+# act = action(99)
+# print(act(3))
+
